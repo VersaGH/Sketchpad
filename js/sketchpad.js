76f853c83bd1b4dfe76465e0c97216b7ba259463
VersaGH <matt-miles@hotmail.com>
2017-03-16 15:39:58 -0500

    Sketchpad created and functioning. Reset button works

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -1,12 +1,37 @@
-
+holdMouse = false;
 
 $(document).ready(function() {
-    for(var x=0; x<48; x++) {
-        for(var y=0; y<48; y++) {
-            var unit = $("div class='unit'></div>");
-            unit.appendTo('#container');            
-                           
+    
+    for (x = 1; x < 48; x++) {
+        $('#container').append('<div class ="unit"></div>');
+        for (y = 1; y < 48; y++) {
+            $('#container').append('<div class ="unit"></div>');
         }
     }
+    
+    $('.unit').mousedown(function() {
+        holdMouse = true;
+        $(this).css('background-color', 'red');
+        mouseHeld();
+
+    }).mouseup(function() {
+        holdMouse = false;
+        $(this).css('background-color', 'blue')
+    });
+    
+    $('#reset').click(function() {
+        $('.unit').css('background-color', 'black');
+        holdMouse = false;
+    });
+        
 });
-                  
+
+function mouseHeld() {
+    if (holdMouse) {
+        $('.unit').mouseenter(function() {
+            $(this).css('background-color', 'red');
+                    
+    })
+    } 
+}
+
