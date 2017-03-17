ff057c422be5aaad29aa5eeae1ec834a4bb471e6
VersaGH <matt-miles@hotmail.com>
2017-03-17 15:56:09 -0500

    sketchpad is complete

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -1,37 +1,69 @@
-holdMouse = false;
+
 
 $(document).ready(function() {
     
-    for (x = 1; x < 48; x++) {
+    for (x = 1; x <= 48; x++) {
         $('#container').append('<div class ="unit"></div>');
-        for (y = 1; y < 48; y++) {
+        for (y = 1; y <= 48; y++) {
             $('#container').append('<div class ="unit"></div>');
         }
     }
     
-    $('.unit').mousedown(function() {
-        holdMouse = true;
-        $(this).css('background-color', 'red');
-        mouseHeld();
-
-    }).mouseup(function() {
-        holdMouse = false;
-        $(this).css('background-color', 'blue')
+    $('.unit').mouseenter(function() {
+        $(this).css('background-color', 'white');
     });
-    
+
     $('#reset').click(function() {
         $('.unit').css('background-color', 'black');
-        holdMouse = false;
+
+    });
+    $('#rainbow').click(function() { 
+
+        $('.unit').mouseenter(function() {
+            var rgbColor = ["red","blue","yellow","green","purple","orange","white"];
+            var rand = rgbColor[Math.floor(Math.random() * rgbColor.length)];
+            $(this).css('background-color', rand);    
+        })
+        
+    });
+        $('#white').click(function() { 
+            $('.unit').mouseenter(function() {
+                $(this).css('background-color', 'white');    
+        })
+        
+    });
+        $('#blue').click(function() { 
+            $('.unit').mouseenter(function() {
+                $(this).css('background-color', 'blue');    
+        })
+        
+    });
+        $('#yellow').click(function() { 
+            $('.unit').mouseenter(function() {
+                $(this).css('background-color', 'yellow');    
+        })
+        
+    });
+        $('#red').click(function() { 
+            $('.unit').mouseenter(function() {
+                $(this).css('background-color', 'red');    
+        })
+        
+    });
+    $('#sizeChange').click(function() {
+        var gridSize = prompt("Enter a number 1-48");
+        $('.unit').width(576/gridSize);
+        $('.unit').height(576/gridSize);
+        $('.unit').css('background-color', 'black');
+        for (x = 1; x <= gridSize; x++) {
+            $('#container').append('<div class ="unit"></div>');
+            for (y = 1; y <= gridSize; y++) {
+                $('#container').append('<div class ="unit"></div>');
+        }
+    }
+        
     });
         
 });
 
-function mouseHeld() {
-    if (holdMouse) {
-        $('.unit').mouseenter(function() {
-            $(this).css('background-color', 'red');
-                    
-    })
-    } 
-}
 
